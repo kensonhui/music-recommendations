1e533052292c39cf1844216c2f60727662dad817
Kenson Hui <kenson.hui22@gmail.com>
2022-08-13 14:23:45 -0400

    Add spotify data

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import spotipy
 import os
-import sys
-sys.path.append(os.path.relpath('./MillionSongCode'))
-import MillionSongCode.hdf5_getters as hdf5_getters
-from hdf5_getters import get_artist_name, open_h5_file_read
 
-million_song_folder_path = './MillionSongSubset'
-assert os.path.isdir(million_song_folder_path), 'Incorrect path'
+spotify_data = pd.read_csv('./spotifydata/data.csv')
+genre_data = pd.read_csv('./spotifydata/data_by_genres.csv')
+data_by_year = pd.read_csv('./spotifydata/data_by_year.csv')
 
-def apply_to_all_files(basedir, func=lambda x: x, ext='.h5'):
-    count = 0
-    for root, dirs, files in os.walk(basedir):
-        for filename in files:
-            if filename.endswith(ext):
-                count += 1
-                func(filename)
-                print(get_artist_name(open_h5_file_read(os.path.join(root, filename))).decode('UTF-8'))
-                if count > 10:
-                    return count
-    return count
-
-print(apply_to_all_files(million_song_folder_path))
+sound_features = ['acousticness', 'danceability', 'speechiness']
+fig = px.line(data_by_year, x='year', y=sound_features)
+fig.show()
